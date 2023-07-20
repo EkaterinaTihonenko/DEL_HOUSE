eb4e7474564bed0bf048de66337d3199e8136074
Ekaterina <ekaterinatihonenko8@gmail.com>
2023-07-20 15:07:57 +0300

    MAIN

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DEL_HOUSE
+# IT-Academy-PROJECT_del_house
+
+# Website development with HTML, CSS & JavaScript
+
+![Image alt](https://github.com/EkaterinaTihonenko/DEL_HOUSE/blob/main/images/1.png)
---
+++ script/script-page-home.js
@@ -0,0 +1,35 @@
+const modal = document.getElementById("modal");
+const modalContentCompletionSending = document.querySelector(
+  ".modal-content-completion-sending"
+);
+const modalContent = document.querySelector(".modal-content");
+const buttonModal = document.getElementById("button-modal");
+const buttonProjects = document.getElementById("button-projects");
+const buttonModalSend = document.querySelector(".btn-form");
+const span = document.getElementsByClassName("close")[0];
+buttonModalSend.onclick = function () {
+  modalContentCompletionSending.style.display = "block";
+  modalContent.style.display = "none";
+  return false;
+};
+buttonModal.onclick = function () {
+  modal.style.display = "block";
+  document.forms[0].reset();
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+buttonProjects.onclick = function () {
+  modal.style.display = "none";
+  modalContent.style.display = "block";
+  modalContentCompletionSending.style.display = "none";
+  document.forms[0].reset();
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.forms[0].reset();
+    modalContent.style.display = "block";
+    modalContentCompletionSending.style.display = "none";
+  }
+};
